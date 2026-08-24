04bb3409a929434b90a81ae0107742423a76af4c
SHAIK MAHAMMED ASRAR <sasrar0523@gmail.com>
2026-08-24 14:15:26 +0530

    Update Array-1.py

--- Array-1.py
+++ Array-1.py
@@ -1,5 +1,5 @@
 # print all pairs in the array
-'''
+
 n = int(input("Enter array size : "))
 arr = list(map(int,input().split()))
 
@@ -145,64 +145,6 @@ arr.sort()
 
 print(arr[-2])
 
-#Write a program to count the number of occurrences of each vowel and consonant in a given string. 
-# Print the vowels first and then consonants in lexicological order along with their counts
- 
-s = input()
-
-vowels = "aeiou"
-
-for ch in vowels:
-    if ch in s:
-        print(ch, "-",s.count(ch))
-
-for ch in sorted(set(s)):
-    if ch not in vowels:
-        print(ch, "-", s.count(ch))
-
-#Find and print the second largest word present in the given string. 
-# Words are separated by spaces, and the length of the word determines its size.
-
-s = input()
-
-words = s.split()
-
-words.sort(key=len)
-
-print(words[-2])
-
-#Given a string s, find the first non-repeating character in it and
-#return its index. If it does not exist, return -1.
-
-s = input()
-
-for i in range(len(s)):
-    if s.count(s[i]) == 1:
-        print(i)
-        break
-
-else:
-    print(-1)
-
-#Given a string, check if it is a valid palindrome ignoring consonant characters.
-# A single string containing lowercase alphabets, numbers, and special characters.
-# True if the string is a valid palindrome after ignoring consonant characters, 
-# Special Characters and Spaces otherwise False.
-
-s = input().lower()
-
-vowels = "aeiou"
-new = ""
-
-for ch in s:
-    if ch in vowels:
-        new += ch
-
-if new == new[::-1]:
-    print("True")
-else:
-    print("False")
-
 Pairs with First Value Smaller
 Description
 Print all pairs of numbers from a given array where the first value is strictly smaller than the second value.
@@ -221,46 +163,6 @@ for i in range(n):
         if arr[i] < arr[j]:
             print(arr[i],arr[j])
 
-# Write a program to find factors of a given number.
-
-# Input Format
-# First line consists of a positive integer n
-
-# Output Format
-# Print the space separated integer factors of given number.
-
-n = int(input())
-
-for i in range(1,n+1):
-    if n % i == 0:
-        print(i,end=" ")
-
-# Write a program to count factors of a given number.
-
-# Input Format
-# First line consists of a positive integer n
-
-# Output Format
-# Print the integer count.
-
-n = int(input())
-
-count = 0
-
-for i in range(1,n+1):
-    if n%i == 0:
-        count += 1
-
-print(count)
-
-#Counts the number of words in a given string[words might be separated by multiple space].
-
-s = input()
-
-words = s.split()
-
-print(len(words))
-
 #Given an array of integers and a value k, write a program to reverse the array starting from index k to the end of the array.
 
 n ,k = map(int,input().split())
@@ -269,10 +171,75 @@ arr = list(map(int,input().split()))
 arr[k:] = arr[k:][::-1]
 
 print(*arr)
-'''
+
+
 #Given an array of integers, your task is to find and print the sum of the largest and smallest elements in the array.
 
 n = int(input())
 arr = list(map(int,input().split()))
 
 print(min(arr) + max(arr))
+
+#Given an array of size N, print all the pairs present in the array.
+
+n = int(input())
+arr = list(map(int,input().split()))
+
+for i in range(n):
+    for j in range(i+1,n):
+        print(arr[i],arr[j])
+
+#Given an array of size N, print the sum of each pair present in the array.
+
+n = int(input())
+arr = list(map(int,input().split()))
+
+for i in range(n):
+    for j in range(i+1,n):
+        print(arr[i]+arr[j])
+
+#Given an array of size N, print the difference of each pair present in the array.
+
+n = int(input())
+arr = list(map(int,input().split()))
+
+if n < 2:
+    print("None")
+else:
+    for i in range(n):
+        for j in range(i+1,n):
+            print(abs(arr[i]-arr[j]))
+
+#Given an array of integers with a size of N, print all the pairs whose sum is less than K.
+
+n = int(input())
+arr = list(map(int,input().split()))
+k = int(input())
+
+for i in range(n):
+    for j in range(i+1,n):
+        if arr[i] + arr[j] < k:
+            print(arr[i],arr[j])
+
+#Given an array of size N, print all the pairs whose first value is greater than K.
+
+n = int(input())
+arr = list(map(int,input().split()))
+k = int(input())
+
+found = 
+
+for i in range(n):
+    for j in range(i+1,n):
+        if arr[i] > k:
+            print(arr[i],arr[j])
+
+
+
+
+
+
+
+
+
+
